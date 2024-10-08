1bae60a5157c61691e23fdbcccb3c45433f99bf9
Aarzoo0 <aarjoochougule.scoe.it@gmail.com>
2024-10-08 16:51:04 +0530

    final commit

---
+++ src/main/java/com/ust/ExternalWeatherAPI/model/Weather.java
@@ -0,0 +1,25 @@
+package com.ust.ExternalWeatherAPI.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Weather {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String locationName;
+    private String temperature;
+    private String weatherCondition;
+
+
+}
